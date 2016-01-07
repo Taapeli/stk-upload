cb3606d7c7b1d30e84e68928dd7d4834e47846bf
jpek-m <juha.makelainen@iki.fi>
2016-01-07 21:56:12 +0200

    Versio 1b toimii: cvs-tiedosto luetaan ja näytetään taulukkona

--- persdemo/models/loadfile.py
+++ persdemo/models/loadfile.py
@@ -5,6 +5,7 @@
 # JMä 30.12.2015
 
 import os
+import logging
 from flask import Flask, redirect, url_for
 from werkzeug import secure_filename
 
@@ -17,26 +18,33 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def upload_file(infile, fmt='list'):
     """ Save file 'infile' in the upload folder 
     and show as 'list' or 'table' """
-    filename = normalized_name(infile)
-    infile.save(filename)
+    try:
+        filename = normalized_name(infile)
+        kokonimi=fullname(filename)
+    except IOError as e:
+        # 415 Unsupported Media Type
+        return redirect(url_for('virhesivu', code=415, text=str(e)))
+        
+    infile.save(kokonimi)
+    logging.debug('Tiedosto "' + kokonimi + '" talletettu')
     return redirect(url_for('nayta1', filename=filename, fmt=fmt))
 
 def normalized_name(infile):
     """ Tarkastetaan tiedostonimi ja palautetaan täysi polkunimi """
     # Tiedostonimi saatu?
     if not infile:
-        # 404 Not Found
-        return redirect(url_for('virhesivu', code=404, text="tiedostonimi puuttuu"))
+        raise IOError('Tiedostonimi puuttuu')
     # Tiedostopääte ok?
     ok_name = '.' in infile.filename and \
            infile.filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
     if not ok_name:
-        # 415 Unsupported Media Type
-        return redirect(url_for('virhesivu', code=415, text=infile.filename))
+        raise IOError('Tiedostopääte ei ole sallittu')
     # Palautetaan nimi ilman ylimääräisiä hakemistotasoja
     return secure_filename(infile.filename)
 
 def fullname(name):
     """ Palauttaa täyden polkunimen """
-    return os.path.join(app.config['UPLOAD_FOLDER'], name)
- 
+    if not name:
+        return ''
+    else:
+        return os.path.join(app.config['UPLOAD_FOLDER'], name)
--- persdemo/persdemo.py
+++ persdemo/persdemo.py
@@ -2,6 +2,8 @@
 # Taapeli harjoitustyö @ Sss 2016
 # JMä 29.12.2015
 
+import csv
+import logging
 #from __future__ import print_function
 from flask import Flask, render_template, request
 
@@ -19,6 +21,7 @@ def index():
 @app.route('/lataa1a', methods=['POST'])
 def lataa1a(): # Lataa tiedoston ja näyttää sen
     infile = request.files['filenm']
+    logging.debug('Ladataan tiedosto ' + infile.filename)
     return upload_file(infile, fmt='list')
 
 @app.route('/lataa1b', methods=['POST'])
@@ -31,14 +34,50 @@ def nayta1(filename, fmt):   # tiedoston näyttäminen ruudulla
     pathname = fullname(filename)
     try:
         with open(pathname, 'r') as f:
-            read_data = f.read().decode('UTF-8')
+            read_data = f.read().decode('UTF-8')    
     except IOError as e:
         read_data = "(Tiedoston lukeminen ei onnistu" + e.strerror + ")"
-    
+
+    # Vaihtoehto a:
     if fmt == 'list':   # Tiedosto sellaisenaan
         return render_template("lista1.html", name=pathname, data=read_data)
-    else:               # Tiedot taulukkona
-        return render_template("table1.html", name=pathname, data=read_data)
+    
+    # Vaihtoehto b: Luetaan tiedot taulukoksi
+    else:
+        with open(pathname, 'rb') as f:
+            reader = csv.DictReader(f)
+            rivit = []
+
+            for row in reader:
+                # Onko henkilörivi?
+                etu=row['Sukunimi_vakioitu']
+                suku=row['Etunimi_vakioitu']
+                if suku == '' and etu == '':
+                    continue
+
+                if etu == '': etu = 'N'
+                if suku == '': suku = 'N'
+                nimi='%s %s' % (etu, suku)
+
+                """ Käräjät-tieto on yhdessä sarakkeessa tai 
+                    Juhan muuntamana kolmessa: käräjä, alku, loppu
+                """
+                if 'Käräjäpaikka' in row:
+                    karaja = '%s %s...%s' % \
+                         (row['Käräjäpaikka'], row['Alkuaika'], row['Loppuaika'])
+                    if karaja[-3:] == '...':
+                        karaja = karaja[:-3]
+                else:
+                    karaja = row['Käräjät']
+
+                rivi = dict( \
+                    nimi=nimi.decode('UTF-8'), \
+                    ammatti=row['Ammatti_vakioitu'].decode('UTF-8'), \
+                    paikka=row['Paikka_vakioitu'].decode('UTF-8'), \
+                    karajat=karaja.decode('UTF-8'))
+                rivit.append(rivi)
+
+        return render_template("table1.html", name=pathname, rivit=rivit)
 
 
 @app.route('/lataa2', methods=['POST'])
@@ -61,5 +100,6 @@ def virhesivu(code, text=''):
     return render_template("virhe_lataus.html", code=code, text=text)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     app.run(debug=True)
     # app.run(host='0.0.0.0', port=80)
