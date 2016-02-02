4f92c42c38237b7b20f6df46db3e1c84a8b91f03
jpek-m <juha.makelainen@iki.fi>
2016-02-02 16:57:06 +0200

    datareader.datastorer() käyttöön, kirjoittaa kantaan

--- persdemo/models/datareader.py
+++ persdemo/models/datareader.py
@@ -6,13 +6,52 @@ import csv
 import logging
 from models.genealogy import *  # Tietokannan luokat
 
-           
+def _poimi_(row_nro, row, url):
+    """ Poimitaan henkilötiedot riviltä ja palautetaan Person-objektina
+    """
+    person_id = Person.make_id(row_nro)
+    suku=row['Sukunimi_vakioitu']
+    etu=row['Etunimi_vakioitu']
+
+    """ Käräjät-tieto on yhdessä sarakkeessa muodossa 'Tiurala 1666.02.20-22'
+        Paikka erotetaan ja aika muunnetaan muotoon '1666-02-20 … 22'
+        Päivämäärän korjaus tehdään jos kentässä on väli+numero.
+        - TODO Pelkää vuosiluku käräjäpaikkana pitäisi siirtää alkuajaksi
+     """
+    if ' 1' in row['Käräjät']:
+        kpaikka, aika = row['Käräjät'].split(' 1')
+        aika = Date.range_str('1' + aika)
+    else:
+        kpaikka, aika = (row['Käräjät'], '')
+
+    # Luodaan henkilö ja käräjätapahtuma
+
+    p = Person(person_id)
+    p.name = Name(etu, suku)
+    p.occupation = row['Ammatti_vakioitu']
+    p.place=row['Paikka_vakioitu']
+
+    event_id = u'E%06d' % row_nro
+    e = Event(event_id, 'Käräjät')
+    e.name = kpaikka
+    e.date = aika
+
+    c = Citation()
+    c.tyyppi = 'Signum'
+    c.id = row['Signum']
+    c.url = url
+    c.source = Source()
+    c.source.nimi = kpaikka + ' ' + aika
+    e.citation = c
+
+    p.events.append(e)
+    return p
+
 def henkilolista(pathname):
     """ Lukee csv-tiedostosta aineiston, ja luo kustakin 
         syöttörivistä Person-objektit
     """
     persons = []
-    events = {}
     row_nro = 0
     url = ''
 
@@ -25,8 +64,7 @@ def henkilolista(pathname):
                 if not "Käräjät" in reader.fieldnames:
                     raise KeyError('Sarake "Käräjät" puuttuu: ' + str(reader.fieldnames))
             row_nro += 1
-            person_id = Person.make_id(row_nro)
-
+    
             # Onko otsikkorivi? Tästä url kaikille seuraaville riveille
             if row['Käräjät'][:4] == 'http':
                 url = row['Käräjät']
@@ -34,60 +72,52 @@ def henkilolista(pathname):
                 continue
 
             # Onko henkilörivi?
-            suku=row['Sukunimi_vakioitu']
-            etu=row['Etunimi_vakioitu']
-            if suku == '' and etu == '':
+            if row['Sukunimi_vakioitu'] == '' and row['Etunimi_vakioitu'] == '':
                 logging.warning('%s: nimikentät tyhjiä!' % person_id)
                 continue
-
-            """ Käräjät-tieto on yhdessä sarakkeessa muodossa 'Tiurala 1666.02.20-22'
-                Paikka erotetaan ja aika muunnetaan muotoon '1666-02-20 … 22'
-                Päivämäärän korjaus tehdään jos kentässä on väli+numero.
-                - TODO Pelkää vuosiluku käräjäpaikkana pitäisi siirtää alkuajaksi
-             """
-            if ' 1' in row['Käräjät']:
-                kpaikka, aika = row['Käräjät'].split(' 1')
-                aika = Date.range_str('1' + aika)
-            else:
-                kpaikka, aika = (row['Käräjät'], '')
-            
-            # Luodaan henkilö ja käräjätapahtuma
-            
-            p = Person(person_id)
-            p.name = Name(etu, suku)
-            p.occupation = row['Ammatti_vakioitu']
-            p.place=row['Paikka_vakioitu']
-            
-            event_id = u'E%06d' % row_nro
-            e = Event(event_id, 'Käräjät')
-            e.name = kpaikka
-            e.date = aika
-            
-            c = Citation()
-            c.tyyppi = 'Signum'
-            c.id = row['Signum']
-            c.url = url
-            c.source = Source()
-            c.source.nimi = kpaikka + ' ' + aika
-            e.citation = c
-            
-            p.events.append(e)
+                
+            p = _poimi_(row_nro, row, url)
             persons.append(p)
 
-            # Testi2
-
-            p.save()
-
     logging.info(u'%s: %d riviä' % (pathname, row_nro))
-
     return (persons)
 
+
 def datastorer(pathname):
-    """ Lukee csv-tiedostosta aineiston kantaan
+    """ Lukee csv-tiedostosta aineiston, ja tallettaa kustakin 
+        syöttörivistä Person-objektit
     """
-    message = "Pitäisi toteuttaa henkilölistan tapaan, " + \
-              "mutta luetut objektit talletetaan kantaan eikä taulukkoihin"
+    row_nro = 0
+    url = ''
+
+    with open(pathname, 'r', newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f, dialect='excel')
+
+        for row in reader:
+            if row_nro == 0:
+                logging.debug("Tiedosto " + pathname + ", sarakkeet: " + str(reader.fieldnames))
+                if not "Käräjät" in reader.fieldnames:
+                    raise KeyError('Sarake "Käräjät" puuttuu: ' + str(reader.fieldnames))
+            row_nro += 1
     
+            # Onko otsikkorivi? Tästä url kaikille seuraaville riveille
+            if row['Käräjät'][:4] == 'http':
+                url = row['Käräjät']
+                #logging.debug('%s: url=%s' % (person_id, url))
+                continue
+
+            # Onko henkilörivi?
+            if row['Sukunimi_vakioitu'] == '' and row['Etunimi_vakioitu'] == '':
+                logging.warning('%s: nimikentät tyhjiä!' % person_id)
+                continue
+                
+            p = _poimi_(row_nro, row, url)
+    
+            # Tallettaa Person-olion ja siihen sisältyvät Eventit
+            # (Person)-[OSALLISTUU]->(Event)
+            p.save()
+
+    message ='Talletettu %d riviä tiedostosta %s' % (row_nro, pathname)
     return message
 
 def lue_henkilot():
@@ -100,4 +130,4 @@ def lue_henkilot():
 
     # Toteutetaan henkilölistan tapaan, mutta objektit luetaan kannasta
     
-    return ((persons, events))
+    return (persons)
