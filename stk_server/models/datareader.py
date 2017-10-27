43ec5b1db4b34f7792ac906b0042d30947f8e32a
jorma-h <jorma.haapasalo@pp.inet.fi>
2017-10-27 19:48:40 +0300

    Events collected under the page

--- stk_server/models/datareader.py
+++ stk_server/models/datareader.py
@@ -686,28 +686,29 @@ def get_source_with_events(sourceid):
 
     event_list = []
     for record in result:               # Events record
-        c = Citation()
-        c.page = record["page"]
-        pid = record["pid"]
-        c.confidence = record["confidence"]
                 
-        for event in record["events"]:
-            e = Event()
-            e.uniq_id = event[0]
-            e.type = event[1]
-            e.edate = event[2]
+        for citation in record["citations"]:
+            c = Citation()
+            c.page = citation[0]
+            c.confidence = citation[1]
             
-            for name in record["names"]:
-                n = Name()
-                n.uniq_id = pid        
-                n.surname = name[0]        
-                n.firstname = name[1]  
-                    
-                e.names.append(n)
-                      
-            c.events.append(e)
-            
-        event_list.append(c)
+            for event in citation[2]:
+                e = Event()
+                e.uniq_id = event[0]
+                e.type = event[1]
+                e.edate = event[2]
+                
+                for name in event[3]:
+                    n = Name()
+                    n.uniq_id = name[0]        
+                    n.surname = name[1]        
+                    n.firstname = name[2]  
+                        
+                    e.names.append(n)
+                          
+                c.events.append(e)
+                
+            event_list.append(c)
 
     return (s.stitle, event_list)
 
