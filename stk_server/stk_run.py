f3c97dbf4a6fedf4fe956d8fef1f6ea2e1ae9816
jpek-m <jpek@iki.fi>
2017-12-01 22:02:36 +0200

    Ei käynnistetä stk_run.py:stä
    vaan /run.py. .settings pois versionhallinnasta

--- stk_server/stk_run.py
+++ stk_server/stk_run.py
@@ -825,19 +825,8 @@ def talleta(filename, subj):
         except:
             return term + '?'
     
-    #===============================================================================
-    # 
-    # """ ----------------------------- Käynnistys ------------------------------- """
-    # 
-    # if __name__ == '__main__':
-    #     if True:
-    #         # Ajo paikallisesti
-    #         logging.basicConfig(level=logging.DEBUG)
-    #         print ("stk-run.__main__ ajetaan DEGUB-moodissa")
-    #         app.run(debug='DEBUG')
-    #     else:
-    #         # Julkinen sovellus
-    #         logging.basicConfig(level=logging.INFO)
-    #         app.run(host='0.0.0.0', port=8000)
-    #===============================================================================
+    """ ----------------------------- Käynnistys ------------------------------- """
+     
+    if __name__ == '__main__':
+        print("Käynnistys tehdään ohjelmalta /run.py tai /runssl.py")
     
