5464bbf5563a063e357e597f515a3e58a797b4cd
TimNal <timo.nallikari@gmail.com>
2017-12-01 18:18:29 +0200

    /config.py lisätty

---
+++ stk_server/config.py
@@ -0,0 +1,22 @@
+'''
+Created on 17.11.2017
+
+@author: TimNal
+
+Sovelluksen toiminnallisuutta ohjaavat tiedot  
+'''
+  
+print('Luetaan sovelluksen config.py')
+DEBUG = False
+SECURITY_REGISTERABLE = True
+SECURITY_CONFIRMABLE = True
+SECURITY_RECOVERABLE = True
+#SECURITY_REGISTER_URL = '/register'
+#SECURITY_SEND_CONFIRMATION_TEMPLATE = 'security/send_confirmation.html' 
+#SECURITY_LOGIN_USER_TEMPLATE = 'login/logged.html'
+SECURITY_POST_LOGIN_VIEW = '/'
+SECURITY_POST_LOGOUT_VIEW = 'login'
+#SECURITY_REGISTER_USER_TEMPLATE = 'security/register_user.html'
+#SECURITY_RESET_PASSWORD_TEMPLATE = 'security/reset_password.html'
+#SECURITY_CHANGE_PASSWORD_TEMPLATE = 'security/change_password.html'
+SECURITY_SEND_REGISTER_EMAIL = True
