33ec9b45e7868add71496d462691e1243710058c
TimNal <timo.nallikari@gmail.com>
2017-12-17 21:23:32 +0200

    Loki stkserver konfiguroitu

--- run.py
+++ run.py
@@ -5,13 +5,14 @@ import logging
 print('Käynnistys: ' + str(app))
 """ ----------------------------- Käynnistys ------------------------------- """
 
+logging.basicConfig(level=logging.DEBUG, format=('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger = logging.getLogger('stkserver')
+
 if __name__ == '__main__':
     if True:
         # Ajo paikallisesti
-        logging.basicConfig(level=logging.DEBUG)
         print ("stk-run.__main__ ajetaan DEGUB-moodissa")
         app.run(debug='DEBUG')
     else:
         # Julkinen sovellus
-        logging.basicConfig(level=logging.INFO)
         app.run(host='0.0.0.0', port=8000)
