14a704e0b9c8f0ecb97438fc219c12944b07ae40
Kari Kujansuu <kari.kujansuu@gmail.com>
2018-08-15 13:48:24 +0300

    x

--- app/bp/gedcom/gedcom_transform.py
+++ app/bp/gedcom/gedcom_transform.py
@@ -2,7 +2,7 @@
 
 """
 Generic GEDCOM transformer 
-Kari Kujansuu, 2016.
+Kari Kujansuu, 2016. 
 
 The transforms are specified by separate Python modules ("plugins") in the subdirectory "transforms".
 
