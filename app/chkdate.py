dc67e83d3289d2b90b2f944571d95ae6f821a143
Jorma Haapasalo <jorma.haapasalo@pp.inet.fi>
2018-09-18 19:32:20 +0300

    Git version Unknown

--- app/chkdate.py
+++ app/chkdate.py
@@ -6,6 +6,8 @@ from os import path
 def revision_info(src_path, store_dir=None):
 
     # Get git log info
+
+    moment = "Unknown"
     
     month_dict = {"Jan":1,"Feb":2,"Mar":3,"Apr":4, "May":5, "Jun":6,
                   "Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
