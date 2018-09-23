76042e7dccd6f0c24b2f938416ede01264522b47
Kari Kujansuu <kari.kujansuu@gmail.com>
2018-09-23 21:40:31 +0300

    First attempt to show Gedcom info

--- app/bp/gedcom/handlers.py
+++ app/bp/gedcom/handlers.py
@@ -10,6 +10,9 @@ import datetime
 import time
 import subprocess
 
+from re import match
+from collections import OrderedDict
+
 from flask import render_template, request, redirect, url_for, flash, jsonify
 from flask_security import login_required, current_user
 from flask import send_from_directory
@@ -34,6 +37,59 @@ def init_log(logfile):
     except:
         pass
     logging.basicConfig(filename=logfile,level=logging.INFO, format='%(levelname)s:%(message)s')
+
+
+def show_info(input_gedcom, enc):
+    ''' Read gedgom HEAD info and count level 0 items
+        Returns a list of descriptive lines
+     '''
+    msg = []
+    cnt = {}
+    #msg.append(os.path.basename(input_gedcom) + '\n')
+    try:
+        with open(input_gedcom, 'r', encoding=enc) as f:
+            for _ in range(100):
+                ln = f.readline()
+                if ln[:6] in ['2 VERS', '1 NAME', '1 CHAR']:
+                    msg.append(ln[2:])
+                if ln.startswith('1 SOUR'):
+                    msg.append('Source ' + ln[7:-1] + ' ')
+                if ln.startswith('1 GEDC'):
+                    msg.append('Gedcom ')
+                if ln.startswith('2 CONT _COMMAND'):
+                    #print('"' + ln)
+                    msg.append('– ' + ln[16:-1])
+                if ln.startswith('2 CONT _DATE'):
+                    msg.append(ln[12:])
+                if match('0.*SUBM', ln):
+                    msg.append('Submitter ')
+                if match('0.*INDI', ln):
+                    cnt['INDI'] = 1
+                    break
+            ln = '-'
+            while ln:
+                ln = f.readline()[:-1]
+                if ln.startswith('0'):
+                    flds = ln.split(maxsplit=2)
+                    key = flds[-1][:4]
+                    if key in cnt:
+                        cnt[key] = cnt[key] + 1
+                    elif key != 'TRLR':
+                        cnt[key] = 1
+
+    except OSError:     # End of file
+        pass
+    except UnicodeDecodeError as e:
+        msg.append("Väärä merkistö, lisää esim. '--encoding ISO8859-1'")
+    except Exception as e:
+        msg.append( type(e).__name__ + str(e))
+
+    if cnt:
+        msg.append('        count\n')
+    for i in OrderedDict(sorted(cnt.items())):
+        msg.append('{:4} {:8}\n'.format(i, cnt[i]))
+        
+    return ''.join(msg)
 
 def read_gedcom(filename):
     try:
@@ -200,6 +256,7 @@ def gedcom_info(gedcom):
         num_individuals=num_individuals, 
         transforms=transforms,
         metadata=metadata,
+        info=show_info(filename,"utf-8"),
     )
 
 
