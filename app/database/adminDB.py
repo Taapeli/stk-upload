2e27e4d2fcf6e5a2f0b8f80c85a5235e8b6c82f9
Timo Nallikari <timo.nallikari@gmail.com>
2018-10-17 17:58:46 +0300

    Database initializing and administering activity moved from setups.py to adminDB.py. Master role added to roles and applied to master user

---
+++ app/database/adminDB.py
@@ -0,0 +1,198 @@
+# coding: utf-8  
+
+import sys
+from datetime import datetime
+import logging
+logger = logging.getLogger('stkserver') 
+from neo4j.exceptions import CypherSyntaxError, ConstraintError, CypherError
+
+#neo4j config
+import shareds
+
+#inputs
+ROLES = ({'level':'0',  'name':'guest',    'description':'Kirjautumaton käyttäjä rajoitetuin lukuoikeuksin'},
+         {'level':'1',  'name':'member',   'description':'Seuran jäsen täysin lukuoikeuksin'},
+         {'level':'2',  'name':'research', 'description':'Tutkija, joka voi päivittää omaa tarjokaskantaansa'},
+         {'level':'4',  'name':'audit',    'description':'Valvoja, joka auditoi ja hyväksyy ehdokasaineistoja'},
+         {'level':'8',  'name':'admin',    'description':'Ylläpitäjä kaikin oikeuksin'},
+         {'level':'16', 'name':'master',   'description':'Tietokannan pääkäyttäjä, ei sovellusoikeuksia'})
+
+
+class SetupCypher():
+    """ Cypher classes for setup """
+#erase database 
+    delete_database = """
+    MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r
+    """
+    check_role_count = """
+    MATCH (a:Role) RETURN COUNT(a)
+    """
+
+    set_role_constraint = """
+    CREATE CONSTRAINT ON (role:Role) ASSERT role.name IS UNIQUE
+    """
+
+    role_create = """
+    CREATE (role:Role 
+    {level: $level, name: $name, 
+    description: $description, timestamp: timestamp()})
+    """
+
+    master_check_existence = """
+    MATCH  (user:User) WHERE user.username = 'master' RETURN COUNT(user)
+    """
+        
+    email_val = """
+    MATCH (a:Allowed_email) WHERE a.allowed_email = $email RETURN COUNT(a)
+    """
+
+    set_user_constraint = """
+    CREATE CONSTRAINT ON (user:User) 
+    ASSERT user.username IS UNIQUE
+    """
+            
+    master_create = """
+    MATCH  (role:Role) WHERE role.name = 'master'
+    CREATE (user:User 
+        {username : $username, 
+        password : $password,  
+        email : $email, 
+        name : $name,
+        language : $language, 
+        is_active : $is_active,
+        confirmed_at : timestamp(), 
+        roles : $roles,
+        last_login_at : timestamp(),
+        current_login_at : timestamp(),
+        last_login_ip : $last_login_ip,
+        current_login_ip : $current_login_ip,
+        login_count : $login_count} )           
+        -[:HAS_ROLE]->(role)
+    """ 
+
+#erase total database 
+def delete_database(tx):
+    tx.run(SetupCypher.delete_database)
+
+def roles_exist():
+#  Tarkista roolien olemassaolo
+    print('Check the user roles')
+    num_of_roles = 0
+    results = shareds.driver.session().run(SetupCypher.check_role_count)
+    for result in results:
+        num_of_roles = result[0]
+
+    return(num_of_roles > 0)
+        #inputs
+    
+def create_role(tx, role):
+    try:
+        tx.run(SetupCypher.role_create,
+            level=role['level'],    
+            name=role['name'], 
+            description=role['description'])
+#        tx.commit()            
+#                print(role['name'])
+    except CypherSyntaxError as cex:
+        logger.error('CypherSyntaxError in create_role ' + cex)
+    except CypherError as cex:
+        logger.error('CypherError in create_role ' + cex)
+    except ConstraintError as cex:
+        logger.error('ConstraintError in create_role ' + cex)
+#            print(role['name'])
+
+def create_roles():
+    with shareds.driver.session() as session:
+        for role in ROLES:
+            try:    
+                session.write_transaction(create_role, role)
+                print(role['name'])
+            except CypherSyntaxError as cex:
+                print('Session ', cex)
+                continue
+            except CypherError as cex:
+                print('Session ', cex)
+                continue
+            except ConstraintError as cex:
+                print('Session ', cex)
+                continue
+        print('Roles initialized')
+
+
+def master_exists():
+    print('Check the master user')
+    num_of_masters = 0  
+    for result in shareds.driver.session().run(SetupCypher.master_check_existence):
+        num_of_masters = result[0]
+    return(num_of_masters > 0)    
+
+def build_master_user():
+    return( 
+        {'username': 'master', 
+         'password': 'taapeli',  
+         'email': 'stk.sukututkimusseura@gmail.com', 
+         'name': 'Stk-kannan pääkäyttäjä',
+         'language': 'fi',  
+         'is_active': True,
+         'confirmed_at': datetime.now().timestamp()/1000, 
+         'roles': ['master'],
+         'last_login_at': datetime.now().timestamp()/1000,
+         'current_login_at': datetime.now().timestamp()/1000,
+         'last_login_ip': '127.0.0.1',
+         'current_login_ip': '127.0.0.1',
+         'login_count': 0            
+         } )
+            
+def create_master(master_user):
+    with shareds.driver.session() as session: 
+        try:
+            session.run(SetupCypher.master_create, master_user) 
+        except CypherSyntaxError as cex:
+            logger.error('CypherSyntaxError in create_master ' + cex)
+            return
+        except CypherError as cex:
+            logger.error('CypherError in create_master ' + cex)
+            return
+        except ConstraintError as cex:
+            logger.error('ConstraintError in create_master ' + cex)
+            return
+    logger.info('Master user created')    
+
+
+def create_role_constraints():
+    with shareds.driver.session() as session: 
+        try:
+            session.run(SetupCypher.set_role_constraint)
+        except CypherError as cex:
+            logger.error('CypherError in create_role_constraints ' + cex)
+            return
+        except ConstraintError as cex:
+            logger.error('ConstraintError in create_role_constraints ' + cex)
+            return
+    logger.info('Role costraints created')
+
+
+def create_user_constraints():
+    with shareds.driver.session() as session: 
+        try:  
+            session.run(SetupCypher.set_user_constraint)  
+        except CypherSyntaxError as cex:
+            logger.error('ConstraintError in create_user_constraints ' + cex)
+            return
+        except CypherError as cex:
+            logger.error('ConstraintError in create_user_constraints ' + cex)
+            return
+        except ConstraintError as cex:
+            logger.error('ConstraintError in create_user_constraints ' + cex)
+            return
+    logger.info('User constraints initialized')
+
+
+def initialize_db(): 
+    if not roles_exist():
+        create_role_constraints()
+        create_roles()
+        
+    if not master_exists():
+        create_user_constraints()
+        create_master(build_master_user())
--- app/setups.py
+++ app/setups.py
@@ -7,6 +7,7 @@ from flask_mail import Mail
 from database.models.neo4jengine import Neo4jEngine 
 from database.models.neo4juserdatastore import Neo4jUserDatastore
 from models.gen.dates import DateRange  # Aikavälit ym. määreet
+from database import adminDB
 import shareds
 from chkdate import Chkdate
 from templates import jinja_filters
@@ -129,7 +130,7 @@ class AllowedEmail():
     default_role = ''
     creator = ''
     created_at = ''
-    registered_at = ''
+    confirmed_at = ''
        
     def __init__(self, **kwargs):
         self.allowed_email = kwargs['allowed_email']
@@ -178,150 +179,159 @@ shareds.security = Security(shareds.app, shareds.user_datastore,
     confirm_register_form=ExtendedConfirmRegisterForm)
 
 print('Security set up')
+
 @shareds.security.register_context_processor
 def security_register_processor():
     return {"username": _("Käyttäjänimi"), "name": _("Nimi"), "language": _("Kieli")}
 
-#  Tarkista roolien olemassaolo
-print('Check the user roles')
-num_of_roles = 0
-results = shareds.driver.session().run('MATCH (a:Role) RETURN COUNT(a)')
-for result in results:
-    num_of_roles = result[0]
+adminDB.initialize_db() 
 
-if num_of_roles == 0:
-    #inputs
-    ROLES = ({'level':'0', 'name':'guest', 
-              'description':'Kirjautumaton käyttäjä rajoitetuin lukuoikeuksin'},
-             {'level':'1', 'name':'member', 
-              'description':'Seuran jäsen täysin lukuoikeuksin'},
-             {'level':'2', 'name':'research', 
-              'description':'Tutkija, joka voi päivittää omaa tarjokaskantaansa'},
-             {'level':'4', 'name':'audit', 
-              'description':'Valvoja, joka auditoi ja hyväksyy ehdokasaineistoja'},
-             {'level':'8', 'name':'admin', 
-              'description':'Ylläpitäjä kaikin oikeuksin'},
-             {'level':'16', 'name':'master', 
-              'description':'Tietokannan pääkäyttäjä ilman sovellusoikeuksia'})
-    
-    
-    #functions
-    def create_role_constraints(tx):
-        try:
-            tx.run('CREATE CONSTRAINT ON (role:Role) ASSERT role.name IS UNIQUE')
-            tx.commit() 
-        except CypherSyntaxError as cex:
-            print(cex)
-    
-    def create_role(tx, role):
-        try:
-            tx.run(SetupCypher.role_create,
-                level=role['level'],    
-                name=role['name'], 
-                description=role['description'],
-                timestamp=str(datetime.now()) ) 
-            tx.commit()            
-#                print(role['name'])
-        except CypherSyntaxError as cex:
-            print(cex)
-        except CypherError as cex:
-            print(cex)
-        except ConstraintError as cex:
-            print(cex)
-#            print(role['name'])
-
-    with shareds.driver.session() as session: 
-        session.write_transaction(create_role_constraints)
-    with shareds.driver.session() as session:
-        for role in ROLES:
-            try:    
-                session.write_transaction(create_role, role)
-                print(role['name'])
-            except CypherSyntaxError as cex:
-                print('Session ', cex)
-                continue
-            except CypherError as cex:
-                print('Session ', cex)
-                continue
-            except ConstraintError as cex:
-                print('Session ', cex)
-                continue
-        print('Roles initialized')
-
-print('Check the master user')
-num_of_masters = 0  
-results =  shareds.driver.session().run(SetupCypher.master_check_existence)
-for result in results:
-    num_of_masters = result[0]
-
-if num_of_masters == 0:
-    default_master_params = {'username': 'master', 
-             'password': 'taapeli',  
-             'email': 'stk.sukututkimusseura@gmail.com', 
-             'name': 'Stk-kannan pääkäyttäjä',
-             'language': 'fi',  
-             'is_active': True,
-             'confirmed_at': datetime.now().timestamp(), 
-             'roles': ['admin'],
-             'last_login_at': datetime.now().timestamp(),
-             'current_login_at': datetime.now().timestamp(),
-             'last_login_ip': '127.0.0.1',
-             'current_login_ip': '127.0.0.1',
-             'login_count': 0            
-             }
-     
-
-    def create_user_constraints(tx):
-        try:
-            tx.run(SetupCypher.set_user_constraint)
-            tx.commit() 
-        except CypherSyntaxError as cex:
-            print(cex) 
-            
-    def create_master(tx, user):
-        try:
-            master_params = {
-                'username': user['username'], 
-                'password': user['password'],  
-                'email': user['email'],
-                'name': user['name'],
-                'language': user['language'],  
-                'is_active': True,
-                'confirmed_at': user['confirmed_at'], 
-                'roles': user['roles'],
-                'last_login_at': user['last_login_at'],
-                'current_login_at': user['current_login_at'],
-                'last_login_ip': user['last_login_ip'],
-                'current_login_ip': user['current_login_ip'],
-                'login_count': user['login_count']
-            }
-            tx.run(SetupCypher.master_create, master_params) 
-#                timestamp=str(datetime.now()) ) 
-            tx.commit()            
-#                print(role['name'])
-        except CypherSyntaxError as cex:
-            print(cex)
-        except CypherError as cex:
-            print(cex)
-        except ConstraintError as cex:
-            print(cex)
-
-     
-    print('Set the constraints and master user')
-    with shareds.driver.session() as session: 
-        session.write_transaction(create_user_constraints)
-
-    with shareds.driver.session() as session:
-        try: 
-            session.write_transaction(create_master, default_master_params)
-            print('Master user created')
-           
-        except CypherSyntaxError as cex:
-            print('Session ', cex)
-        except CypherError as cex:
-            print('Session ', cex)
-        except ConstraintError as cex:
-            print('Session ', cex)
+# ================================================================================
+#
+#   Following functionality is moved to database.adminDB
+#
+# ================================================================================
+ 
+# #  Tarkista roolien olemassaolo
+# print('Check the user roles')
+# num_of_roles = 0
+# results = shareds.driver.session().run('MATCH (a:Role) RETURN COUNT(a)')
+# for result in results:
+#     num_of_roles = result[0]
+# 
+# if num_of_roles == 0:
+#     #inputs
+#     ROLES = ({'level':'0', 'name':'guest', 
+#               'description':'Kirjautumaton käyttäjä rajoitetuin lukuoikeuksin'},
+#              {'level':'1', 'name':'member', 
+#               'description':'Seuran jäsen täysin lukuoikeuksin'},
+#              {'level':'2', 'name':'research', 
+#               'description':'Tutkija, joka voi päivittää omaa tarjokaskantaansa'},
+#              {'level':'4', 'name':'audit', 
+#               'description':'Valvoja, joka auditoi ja hyväksyy ehdokasaineistoja'},
+#              {'level':'8', 'name':'admin', 
+#               'description':'Ylläpitäjä kaikin oikeuksin'},
+#              {'level':'16', 'name':'master', 
+#               'description':'Tietokannan pääkäyttäjä ilman sovellusoikeuksia'})
+#     
+#     
+#     #functions
+#     def create_role_constraints(tx):
+#         try:
+#             tx.run('CREATE CONSTRAINT ON (role:Role) ASSERT role.name IS UNIQUE')
+#             tx.commit() 
+#         except CypherSyntaxError as cex:
+#             print(cex)
+#     
+#     def create_role(tx, role):
+#         try:
+#             tx.run(SetupCypher.role_create,
+#                 level=role['level'],    
+#                 name=role['name'], 
+#                 description=role['description'],
+#                 timestamp=str(datetime.now()) ) 
+#             tx.commit()            
+# #                print(role['name'])
+#         except CypherSyntaxError as cex:
+#             print(cex)
+#         except CypherError as cex:
+#             print(cex)
+#         except ConstraintError as cex:
+#             print(cex)
+# #            print(role['name'])
+# 
+#     with shareds.driver.session() as session: 
+#         session.write_transaction(create_role_constraints)
+#     with shareds.driver.session() as session:
+#         for role in ROLES:
+#             try:    
+#                 session.write_transaction(create_role, role)
+#                 print(role['name'])
+#             except CypherSyntaxError as cex:
+#                 print('Session ', cex)
+#                 continue
+#             except CypherError as cex:
+#                 print('Session ', cex)
+#                 continue
+#             except ConstraintError as cex:
+#                 print('Session ', cex)
+#                 continue
+#         print('Roles initialized')
+# 
+# print('Check the master user')
+# num_of_masters = 0  
+# results =  shareds.driver.session().run(SetupCypher.master_check_existence)
+# for result in results:
+#     num_of_masters = result[0]
+# 
+# if num_of_masters == 0:
+#     default_master_params = {'username': 'master', 
+#              'password': 'taapeli',  
+#              'email': 'stk.sukututkimusseura@gmail.com', 
+#              'name': 'Stk-kannan pääkäyttäjä',
+#              'language': 'fi',  
+#              'is_active': True,
+#              'confirmed_at': datetime.now().timestamp(), 
+#              'roles': ['admin'],
+#              'last_login_at': datetime.now().timestamp(),
+#              'current_login_at': datetime.now().timestamp(),
+#              'last_login_ip': '127.0.0.1',
+#              'current_login_ip': '127.0.0.1',
+#              'login_count': 0            
+#              }
+#      
+# 
+#     def create_user_constraints(tx):
+#         try:
+#             tx.run(SetupCypher.set_user_constraint)
+#             tx.commit() 
+#         except CypherSyntaxError as cex:
+#             print(cex) 
+#             
+#     def create_master(tx, user):
+#         try:
+#             master_params = {
+#                 'username': user['username'], 
+#                 'password': user['password'],  
+#                 'email': user['email'],
+#                 'name': user['name'],
+#                 'language': user['language'],  
+#                 'is_active': True,
+#                 'confirmed_at': user['confirmed_at'], 
+#                 'roles': user['roles'],
+#                 'last_login_at': user['last_login_at'],
+#                 'current_login_at': user['current_login_at'],
+#                 'last_login_ip': user['last_login_ip'],
+#                 'current_login_ip': user['current_login_ip'],
+#                 'login_count': user['login_count']
+#             }
+#             tx.run(SetupCypher.master_create, master_params) 
+# #                timestamp=str(datetime.now()) ) 
+#             tx.commit()            
+# #                print(role['name'])
+#         except CypherSyntaxError as cex:
+#             print(cex)
+#         except CypherError as cex:
+#             print(cex)
+#         except ConstraintError as cex:
+#             print(cex)
+# 
+#      
+#     print('Set the constraints and master user')
+#     with shareds.driver.session() as session: 
+#         session.write_transaction(create_user_constraints)
+# 
+#     with shareds.driver.session() as session:
+#         try: 
+#             session.write_transaction(create_master, default_master_params)
+#             print('Master user created')
+#            
+#         except CypherSyntaxError as cex:
+#             print('Session ', cex)
+#         except CypherError as cex:
+#             print('Session ', cex)
+#         except ConstraintError as cex:
+#             print('Session ', cex)
 
 
 """ 
