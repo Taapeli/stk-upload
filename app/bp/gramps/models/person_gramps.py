c0d878b0db9f92ea76c880c97267c03bcc9563d0
Juha <juha.makelainen@iki.fi>
2018-10-19 00:05:01 +0300

    Fixed xml-load

--- app/bp/gramps/models/person_gramps.py
+++ app/bp/gramps/models/person_gramps.py
@@ -112,6 +112,8 @@ class Person_gramps(Person):
             for res in result:
                 self.uniq_id = res[0]
                 print("Person {} ".format(self.uniq_id))
+            if self.uniq_id == None:
+                print("Person <MISSING uniq_id> {}".format(p_attr))
 
         except Exception as err:
             print("Virhe (Person.save:Person): {0}".format(err), file=stderr)
