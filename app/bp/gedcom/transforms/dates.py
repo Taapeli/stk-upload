436243cbda7b69de9a15e21ef203350760383efa
Kari Kujansuu <karikujansuu@gmail.com>
2019-03-04 15:58:26 +0200

    Dates transformation for "dd.mm.yyyy"

--- app/bp/gedcom/transforms/dates.py
+++ app/bp/gedcom/transforms/dates.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
+import datetime
 from flask_babelex import _
 
 from .. import transformer
+
+import re
 
 version = "1.0"
 name = _("Dates")
@@ -15,6 +17,13 @@ def add_args(parser):
 def initialize(options):
     return Dates()
 
+def fmtdate(y,m,d):
+    try:
+        dt = datetime.date(y,m,d)
+        return dt.strftime("%d %b %Y").upper()
+    except:
+        return None
+
 class Dates(transformer.Transformation):
     def transform(self,item,options,phase):
         """
@@ -23,6 +32,19 @@ class Dates(transformer.Transformation):
         31.12.1888 -> 31 DEC 1888
         """
         if item.tag == "DATE":
-            pass
+            # 31.12.1888 -> 31 DEC 1888
+            r = re.match(r"(?P<dd>\d{1,2})\."
+                         r"(?P<mm>\d{1,2})\."
+                         r"(?P<yyyy>\d{4})",item.value.strip())
+            if r:
+                print(item.value)
+                y = int(r.group('yyyy'))
+                m = int(r.group('mm'))
+                d = int(r.group('dd'))
+                val = fmtdate(y,m,d)
+                if val:
+                    item.value = val
+                    return item
+
         return True
 
