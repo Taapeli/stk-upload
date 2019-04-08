685aa6dc049908126194730c9b04f918dc2017bf
Juha <juha.makelainen@iki.fi>
2019-04-08 17:33:13 +0300

    Gramps upload with research role
    + member role didn't work!
    + regresh-button added to uploading file row

--- app/bp/gramps/routes.py
+++ app/bp/gramps/routes.py
@@ -22,14 +22,14 @@ from ..admin import uploads
 
 @bp.route('/gramps')
 @login_required
-@roles_accepted('member', 'admin')
-def gramps_upload():
+@roles_accepted('research', 'admin')
+def gramps_index():
     """ Home page gramps input file processing """
-    print("-> bp.start.routes.gramps_upload")
+    print("-> bp.start.routes.gramps_index")
     return render_template("/gramps/index_gramps.html")
 
 @bp.route('/gramps/show_log/<xmlfile>')
-@roles_accepted('member')
+@roles_accepted('research')
 def show_upload_log(xmlfile):
     upload_folder = uploads.get_upload_folder(current_user.username)
     fname = os.path.join(upload_folder,xmlfile + ".log")
@@ -37,13 +37,13 @@ def show_upload_log(xmlfile):
     return render_template("/admin/load_result.html", msg=msg)
 
 @bp.route('/gramps/uploads')
-@roles_accepted('member', 'admin')
+@roles_accepted('research', 'admin')
 def list_uploads():
     upload_list = uploads.list_uploads(current_user.username) 
     return render_template("/gramps/uploads.html", uploads=upload_list)
 
 @bp.route('/gramps/upload', methods=['POST'])
-@roles_accepted('member', 'admin')
+@roles_accepted('research', 'admin')
 def upload_gramps(): 
     """ Load a gramps xml file to temp directory for processing in the server
     """
@@ -78,28 +78,28 @@ def upload_gramps():
 
 @bp.route('/gramps/start_upload/<xmlname>')
 @login_required
-@roles_accepted('member')
+@roles_accepted('research')
 def start_load_to_neo4j(xmlname):
     uploads.initiate_background_load_to_neo4j(current_user.username,xmlname)
     flash(_('Data import from {!r} to database has been started.'.format(xmlname)), 'info')
     return redirect(url_for('gramps.list_uploads'))
 
 @bp.route('/gramps/virhe_lataus/<int:code>/<text>')
-@roles_accepted('member', 'admin')
+@roles_accepted('research', 'admin')
 def error_page(code, text=''):
     """ Virhesivu näytetään """
     logging.debug('Virhesivu ' + str(code) )
     return render_template("virhe_lataus.html", code=code, text=text)
 
 @bp.route('/gramps/xml_delete/<xmlfile>')
-@roles_accepted('member', 'admin')
+@roles_accepted('research', 'admin')
 def xml_delete(xmlfile):
     uploads.delete_files(current_user.username,xmlfile)
     syslog.log(type="gramps file deleted",file=xmlfile)
     return redirect(url_for('gramps.list_uploads'))
 
 @bp.route('/gramps/xml_download/<xmlfile>')
-@roles_accepted('member', 'admin')
+@roles_accepted('research', 'admin')
 def xml_download(xmlfile):
     xml_folder = uploads.get_upload_folder(current_user.username)
     xml_folder = os.path.abspath(xml_folder)
