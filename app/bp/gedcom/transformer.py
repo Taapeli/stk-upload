dcd5a044497fd2f25a00a3aa7b795a6a9b69f09b
Juha <juha.makelainen@iki.fi>
2019-06-05 14:12:15 +0300

    Continuing gedcom Person_names fix
    - Can be run, but does no output changes

--- app/bp/gedcom/transformer.py
+++ app/bp/gedcom/transformer.py
@@ -143,20 +143,20 @@ class Item:
         - value         str    text following tag "F" etc
                         int    x
     """
-    def __init__(self,line,children=None,lines=None,linenum=None):
+    def __init__(self, line, children=None, lines=None, linenum=None):
         if children is None: 
             children = []
         self.linenum = linenum
-        temp = line.split()
-        if len(temp) < 2: 
+        parts = line.split()
+        if len(parts) < 2: 
             raise RuntimeError(_("Invalid line: ") + line)
-        self.level = int(temp[0])
-        if self.level == 0 and temp[1][0] == '@':
-            self.xref = temp[1]
-            self.tag = temp[2]
+        self.level = int(parts[0])
+        if self.level == 0 and parts[1][0] == '@':
+            self.xref = parts[1]
+            self.tag = parts[2]
         else:
             self.xref = ""
-            self.tag = temp[1]
+            self.tag = parts[1]
         self._line = line
         self.children = children
         self.lines = lines
@@ -221,20 +221,19 @@ class Transformer:
         return items
     
     def transform_items(self, items, path="", phase=1):
-        """ Do Item transformation.
+        """ Do transformation for a list of Items.
+        
+            This is called recursive for inner level Items.
         """
         newitems = []
         for item in items:
-            #print(f"##Item {item.linenum}: {item} (+{len(item.children)})\n")
-            if path: 
-                item.path = path + "." + item.tag
-            elif item.xref:
-                item.path = item.xref + "." + item.tag
-            else:
-                item.path = item.tag
+            if path:        item.path = path + "." + item.tag
+            elif item.xref: item.path = item.xref + "." + item.tag
+            else:           item.path = item.tag
             # Process the children (lines with next higher level numbers)
             item.children = self.transform_items(item.children, path=item.path, phase=phase)
             newitem = self.transformation.transform(item,self.options,phase)
+            print(f"# {item.line[:6]} – {len(item.children)} children")
             if newitem == True: # no change
                 newitems.append(item)
                 continue
--- app/bp/gedcom/transforms/person_names.py
+++ app/bp/gedcom/transforms/person_names.py
@@ -14,6 +14,8 @@
 
 Created on 26.11.2016 – 2.6.2019
 
+    Converted from bp.gedcom.transforms.names
+
 @author: JMä
 '''
 #     Input example (originally no indent):
@@ -74,19 +76,16 @@ class PersonNames(transformer.Transformation):
         Note: If you change the item in this function but still return True, then the changes
         are applied to the Gedcom but they are not displayed with the --display-changes option.
         """
-        if True:    # options.remove_invalid_marriage_dates:
-            logger.debug(f"#Item {item.linenum}: {item}")
-            if item.line.strip() == "1 MARR":
-                # replace
-                #     1 MARR
-                #     2 DATE AVOLIITTO
-                # with
-                #     1 MARR
-                #     2 TYPE AVOLIITTO
-                if len(item.children) > 0 and item.children[0].line.startswith("2 DATE AVOLIITTO"):
-                    item.children[0] = Item("2 TYPE AVOLIITTO")
-                    return item
-                return True # no change
+        print(f"#Item {item.linenum}: {item} ({len(item.children)} included)")
+        if item.path.find('.INDI') < 0:
+            return True
+
+        if item.tag == "NAME":
+            print(f"## Name {item.value} conversion")
+            logger.debug(f"##Item {item.linenum}: {item}")
+            n = PersonName(item)
+            n.process_name_item(False)
+            return n
 
         if False:   #options.remove_multiple_blanks: # 2.2.3
             if item.tag in ('NAME','PLAC'):
@@ -125,182 +124,4 @@ class PersonNames(transformer.Transformation):
 
 
         return True # no change
-
-#-------------------------------------------------------------------------------
-
-# def initialize(run_args):
-#     global indi_record
-#     global state            
-#     state = 0
-#     indi_record = None
-
-def phase3(run_args, gedline, f):
-    '''
-    Function phase3 is called once for each line in the input GEDCOM file.
-    This function produce the output GEDCOM by calling output_file.emit() for each line.
-    If an input line is not modified then the original lines are emitted as is.
-
-    Arguments example:
-        run_args={'display_changes': False, 'dryrun': True, 'encoding': 'utf-8', \
-                  'input_gedcom': ../Mun-testi.ged', 'transform': 'names'}
-        gedline=(
-            line='1 NAME Antti /Puuhaara/'
-            level=1
-            path='@I0001@.NAME'
-            tag='NAME'
-            value='Antti /Puuhaara/'
-        )
-        f=<__main__.Output object at 0x101960fd0>
-    '''
-
-    global logical_record
-    global state
-    #print("# Phase3: run_args={!r}, line={!r}, path={!r}, tag={!r}, value={!r}, f={!r}".\
-    #      format(run_args,line,path,tag,value,f))
-
-    ''' 
-    ---- INDI automation engine for processing person data ----
-         See automation rules below 
-    '''
-
-    # For all states
-    if gedline.level == 0:
-        if gedline.value == 'INDI':  # Start new INDI
-            # "0 INDI" starts a new logical record
-            _T1_emit_and_create_record(gedline, f)
-            state = 1
-        else:
-            # 0 level line ends previous logical record, if any and
-            # starts a non-INDI logical record, which is emitted as is
-            _T2_emit_record_and_gedline(gedline, f)
-            state = 0
-        return
-    
-    # For all but "0 INDI" lines
-    if state == 0:      # Started, no active INDI
-        # Lines are emitted as is
-        _T3_emit_gedline(gedline, f)
-        return
-
-    if state == 1:      # INDI processing active
-        if gedline.level == 1:
-            if _is_gedline_a_NAME(gedline):
-                # Start a new PersonName in GedcomRecord
-                _T4_store_name(gedline)
-                state = 2
-                return
-
-            if gedline.tag == 'BIRT':
-                state = 3
-
-        # Higher level lines are stored as a new members in the INDI logical record
-        _T6_store_member(gedline)
-        return
-    
-    if state == 2:      # NAME processing active in INDI
-        if gedline.level == 1:
-            # Level 1 lines terminate current NAME group
-            if _is_gedline_a_NAME(gedline):
-                # Start a new PersonName in GedcomRecord
-                _T4_store_name(gedline)
-                state = 2
-                return
-            # Other level 1 lines terminate NAME and are stored as INDI members
-            if gedline.tag == 'BIRT':
-                state = 3
-            else:
-                state = 1
-            _T6_store_member(gedline)
-        else:
-            # Higher level lines are stored as a new members in the latest NAME group
-            _T7_store_name_member(gedline)
-        return
-
-    if state == 3:       # BIRT processing (to find birth date) active in INDI
-        if gedline.level == 2 and gedline.tag == 'DATE':
-            _T5_save_date(gedline,'BIRT')
-            state = 1
-            return
-        if gedline.level == 1:
-            if _is_gedline_a_NAME(gedline):
-                # Start a new PersonName in GedcomRecord
-                _T4_store_name(gedline)
-                state = 2
-            else:
-                _T6_store_member(gedline)
-                state = 1
-            return
-        # Level > 1, still waiting DATE
-        _T6_store_member(gedline)
-        return
-
-'''
-# ---- Automation rules ----
-#                             1 ALIA
-# state \ input!!0 INDI!0 ... !1 NAME !1 BIRT !2 DATE !2,3,4, !1 ... ! end
-#--------------++------+------+-------+-------+-------+------+-----
-# 0  "Started" || 1,T1 | 0,T3 | 0,T3  | 0,T3  | 0,T3  | 0,T3  | 0,T3 | 0,T2
-# 1  "INDI"    || 1,T1 | 0,T2 | 2,T4  | 3,T6  | 1,T6  | 1,T6  | 1,T6 | 0,T2
-# 2  "NAME"    || 1,T1 | 0,T2 | 2,T4  | 3,T6  | 2,T7  | 2,T7  | 1,T6 | 0,T2
-# 3  "BIRT"    || 1,T1 | 0,T2 | 2,T4  | 1,T6  | 1,T5  | 3,T6  | 1,T6 | 0,T2
- For example rule "2,T4" means operation T4 and new state 2.
-'''
-
-def _T1_emit_and_create_record(gedline, f):
-    ''' Emit previous logical person record (if any) and create a new one '''
-    global indi_record
-    if indi_record != None:
-        indi_record.emit(f)
-    # Create new logical record
-    indi_record = GedcomRecord(gedline)
-
-def _T2_emit_record_and_gedline(gedline, f):
-    ''' Emit previous logical person record (if any) and emit line '''
-    global indi_record
-    if indi_record != None:
-        indi_record.emit(f)
-        indi_record = None
-    # Emit current line to output file
-    gedline.emit(f)
-
-def _T3_emit_gedline(gedline, f): 
-    ''' Emit current line '''
-    gedline.emit(f)
-
-def _T4_store_name(gedline):
-    ''' Save gedline as a new PersonName to the logical person record '''
-    global indi_record
-    if gedline.tag == 'ALIA':
-        # For an ALIA line: 1) Change tag to 'NAME' 2) add line '_orig_ALIA'
-        nm = PersonName(gedline)
-        nm.tag = 'NAME'
-        noteline = GedcomLine((gedline.level + 1, 'NOTE', '_orig_ALIA' + gedline.value))
-        nm.add_line(noteline)
-    else: # Real 'NAME'
-        nm = PersonName(gedline)
-    indi_record.add_member(nm)
-
-def _T5_save_date(gedline, tag):
-    ''' Pick year from gedline and store current gedline '''
-    global indi_record
-    indi_record.store_date(gedline.get_year(),tag)
-    indi_record.add_member(gedline)
-    
-def _T6_store_member(gedline):
-    ''' Save a new gedline member to the logical record '''
-    global indi_record
-    indi_record.add_member(gedline)
-
-def _T7_store_name_member(gedline):
-    ''' Save current line to the current name object '''
-    global indi_record
-    indi_record.get_nameobject().add_line(gedline)
-
-
-def _is_gedline_a_NAME(gedline):
-    ''' Check if this is a NAME line or a ALIA line with no @...@ reference.
-        (This kind of ALIA will be changed to NAME when outputting PersonName)
-    '''
-    return gedline.tag == 'NAME' or \
-        (gedline.tag == 'ALIA' and not gedline.value.startswith('@'))
 
