68f433eec1dc956136a9bdb66e4a4cd2709dd65b
Kari Kujansuu <karikujansuu@gmail.com>
2019-09-09 15:16:54 +0300

    Added roles to sitemap

--- app/models/util.py
+++ app/models/util.py
@@ -1,5 +1,8 @@
 import os
 import time
+from pprint import pprint
+import ast
+import _ast
 
 def generate_name(name):
     """
@@ -42,3 +45,63 @@ def guess_encoding(fname):
         except UnicodeDecodeError:
             pass
     return None
+
+from dataclasses import dataclass
+
+
+def scan_endpoints_for_file(fname):    
+    source = open(fname).read()
+    root = ast.parse(source)
+
+    @dataclass
+    class Info: 
+        name: str
+        urls: list = None
+        login_required: bool = False
+        roles_accepted: str = None
+        roles_required: str = None
+
+    endpoints = {}
+    for node in ast.iter_child_nodes(root):
+        if hasattr(node,'decorator_list'):
+            #print(node.name,node.decorator_list)
+            info = Info(node.name)
+            
+            for call in node.decorator_list:
+                if isinstance(call,_ast.Name):
+                    if call.id == "login_required":
+                        info.login_required = True
+                if isinstance(call,_ast.Call):
+                    if isinstance(call.func,_ast.Attribute):
+                        decorator_name = call.func.attr
+                    if isinstance(call.func,_ast.Name):
+                        decorator_name = call.func.id
+                    arglist = [arg.s for arg in call.args]
+                    args = ",".join(arglist)
+                    if decorator_name == 'route':
+                        if len(arglist) != 1:
+                            raise RuntimeError("Invalid route "+ arglist[0])
+                        if info.urls is None: info.urls = []
+                        info.urls.append(arglist[0])
+                    if decorator_name == 'roles_accepted':
+                        info.roles_accepted = arglist
+                    if decorator_name == 'roles_required':
+                        info.roles_required = arglist
+            if info.urls is not None:
+                for url in info.urls:
+                    endpoints[url] = info
+    return endpoints
+
+def scan_endpoints():
+    endpoints = {}
+    for dirname,dirs,files in os.walk("app"):
+        for file in files:
+            if file.endswith(".py"):
+                fname = os.path.join(dirname,file)
+                endpoints1 = scan_endpoints_for_file(fname)
+                #pprint(endpoints)   
+                endpoints.update(endpoints1)     
+    return endpoints
+
+
+
