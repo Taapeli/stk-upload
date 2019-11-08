0b5bc074be9d6b8e6960e93668f3d9640278896a
Juha <juha.makelainen@iki.fi>
2019-11-08 20:24:02 +0200

    listCitations and dummy setCitation

--- app/static/js/scene_scripts.js
+++ app/static/js/scene_scripts.js
@@ -3,6 +3,7 @@ var _ = function(msgid,args) { return gt.strargs(gt.gettext(msgid),args); };
 var ngettext = function(msgid, msgid_plural, n) { return gt.ngettext(msgid, msgid_plural, n); };
 	
 function jump(initial) {
+	// Jump to given #id and scroll a bit back
 	var elem = $("a[name="+initial+"]");
     var rect = elem.get(0).getBoundingClientRect();
 	var y = rect.top;
@@ -12,12 +13,21 @@ function jump(initial) {
 
 var cit_tbl = ["rivi1", "rivi2"];	//Table of citations (for person.html etc)
 
-function showTable() {
+function listCitations(tbl) {
 	// Display cit_tbl table
-	var t = document.getElementById("demo");
-	t.innerHTML = "<tr><th>Citation table type is " + typeof cit_tbl + "</th><tr>";
-	for (i = 0; i < cit_tbl.length; i++) { 
-		t.innerHTML += "<tr><td>rivi x</td><td>" + cit_tbl[i] + "</td><tr>";
+	var t = document.getElementById(tbl);
+	t.innerHTML = "<tr><th>mark</th><th>source</th><th>citation</th></th><tr>";
+	for (i = 0; i < cit_tbl.length; i++) {
+		if (cit_tbl[i].length == 3) {
+			t.innerHTML += "<tr><td>" + cit_tbl[i][2] + "</td><td>" 
+				+ cit_tbl[i][0] + "</td><td>" + cit_tbl[i][1] + "</td><tr>";
+		} else {
+			t.innerHTML += "<tr><td>rivi x</td><td>" + cit_tbl[i] + "</td><tr>";
+		}
 	}
 	console.log("Citation table=" + cit_tbl);
+}
+
+function setCitation(cit) {
+	// Finds or creates next mark symbol for given citation
 }
