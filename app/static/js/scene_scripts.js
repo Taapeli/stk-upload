b12f23ad99cdc60affccc11d8df6196e34c0e31c
Juha <juha.makelainen@iki.fi>
2019-11-10 12:31:04 +0200

    New js CitTable
    not in use yet

--- app/static/js/scene_scripts.js
+++ app/static/js/scene_scripts.js
@@ -11,44 +11,71 @@ function jump(initial) {
 }
 
 
-var cit_tbl = [];	//Table of citations (for person.html etc)
+function citTable() {
+	// Manage citations table and indexes "1a", ... for each.
+	this.cTbl = [];
 
-function listCitations(tbl) {
-	// Display cit_tbl table
-	var t = document.getElementById(tbl);
-	t.innerHTML = "<tr><th>mark</th><th>source</th><th>citation</th></th><tr>";
-	for (i = 0; i < cit_tbl.length; i++) {
-		if (cit_tbl[i].length == 3) {
-			t.innerHTML += "<tr><td>" + cit_tbl[i][2] + "</td><td>" 
-				+ cit_tbl[i][0] + "</td><td>" + cit_tbl[i][1] + "</td><tr>";
-		} else {
-			t.innerHTML += "<tr><td>rivi x</td><td>" + cit_tbl[i] + "</td><tr>";
+	this.getMark = function (s_id, c_id) {
+		// Todo
+	    return (s_id + 1) + "abcderfghijklmopqrstuvwxyzåäö"[c_id];
+	}
+
+	this.add = function(s_id, c_id) {
+		// Adds a citation to source.
+		// The lines of this.cTbl are arrays [source_id, [citation_id, ...]]
+	    //this.mark = (s_id + 1) + "abcderfghijklmopqrstuvwxyzåäö"[c_id];
+		var line;
+		var l = this.cTbl.length;
+		for (i = 0; i < l; i++) {
+			line = this.cTbl[i];
+			if (line[0] == s_id) {
+				// Matching source
+				var cits = line[1];
+				var a = cits.indexOf(c_id);
+				if (a < 0) {
+					// No match; add a new citation to this source
+					this.cTbl[i][1].push(c_id);
+				}
+				return this.cTbl[i];
+			}
 		}
+		line = [s_id, [c_id]];
+		this.cTbl.push(line);
 	}
-	console.log("Citation table=" + cit_tbl);
-}
 
-function getCitations(tbl) {
-	var x = document.getElementsByTagName("sup");
-	var i;
-	var ret = "";
-	for (i = 0; i < x.length; i++) {
-	    ret += " " + x[i].innerText;
-		a = x[i].firstElementChild;
-		if (a.nodeName == "A") {
-		    ret += ">" + a.id;
+	this.lister = function(destination) {
+		// Display citations table in destination element.
+
+		var t = document.getElementById(destination);
+		t.innerHTML = "<tr><th>source</th><th>citation</th></th><tr>";
+
+		var l = this.cTbl.length;
+		for (i = 0; i < l; i++) {
+			line = this.cTbl[i];
+			t.innerHTML += "<tr><td>" + line[0] + "</td><td>" 
+				+ line[1] + "</td><tr>";
 		}
+		console.log("Citation table=" + this.cTbl);
 	}
-	document.getElementById("demo").innerHTML = ret;
-	listCitations(tbl)
+
+	this.findCitations = function(textDest, tableDest) {
+		// Search html <sup> tags and display the citations in textDest and
+		// table in tableDest.
+		// + Add citations to citTable.
+		var x = document.getElementsByTagName("sup");
+		var i;
+		var ret = "";
+		for (i = 0; i < x.length; i++) {
+		    ret += " " + x[i].innerText;
+			var a = x[i].firstElementChild;
+			if (a.nodeName == "A" && a.id != "" ) {
+			    ret += ">" + a.id + '<br>';
+			    var arr = a.id.split('-');
+			    this.add(Number(arr[0]), Number(arr[1]));
+			} else { ret += '<br>' }
+		}
+		document.getElementById(textDest).innerHTML = ret;	// Text result
+		this.lister(tableDest)								// Table result
+	}
 }
 
-function setCitation(cits, sou) {
-	// Finds or creates mark symbol for given citation
-	ret = []
-	for (cit in cits) {
-		mark = "1a"
-		cit_tbl.append([mark, cit, sou])
-		ret.append(mark)
-	}
-}
