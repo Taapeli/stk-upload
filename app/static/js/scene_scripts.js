7706b9e33abaf6b7ed18d2e6984d69fcd6d64c4f
Juha <juha.makelainen@iki.fi>
2019-11-11 19:42:20 +0200

    All 3 steps as dummy for person citations
    Using citTable js object:
    1. findCitations - pick all rederenced citations, create reference links and store them
    2. listCitations - display citations table
    3. soureReferences - create dummy source/citations list

--- app/static/js/scene_scripts.js
+++ app/static/js/scene_scripts.js
@@ -49,7 +49,7 @@ function citTable() {
 		return this.getMark(this.cTbl.length - 1, 0)
 	}
 
-	this.lister = function(destination) {
+	this.listCitations = function(destination) {
 		// Display citations table in destination element.
 
 		var t = document.getElementById(destination);
@@ -64,10 +64,8 @@ function citTable() {
 		console.log("Citation table=" + this.cTbl);
 	}
 
-	this.findCitations = function(textDest, tableDest) {
-		// Search html <sup> tags and display the citations in textDest and
-		// table in tableDest.
-		// + Add citations to citTable.
+	this.findCitations = function(textDest) {
+		// Search html <sup> tags and add the citations to this.cTbl.
 		var x = document.getElementsByTagName("sup");
 		var i;
 		var ret = "";
@@ -84,7 +82,76 @@ function citTable() {
 			} else { ret += '<br>' }
 		}
 		document.getElementById(textDest).innerHTML = ret;	// Text result
-		this.lister(tableDest)								// Table result
+	}
+
+	this.sourceReferences = function(destination) {
+		// Display citations table in destination element.
+
+		var t = document.getElementById(destination);
+		this.cTbl.sort(function(a, b){return a[0]-b[0]});
+		
+		var l = this.cTbl.length;
+		var source_id = -1;
+		for (i = 0; i < l; i++) {
+			line = this.cTbl[i];
+			if (line[0] != source_id) {
+				// New Source
+				source_id = line[0];
+				var nodeA = document.createElement("A");
+				var textnode = document.createTextNode(" Lähde " + source_id);
+				nodeA.href = '/scene/source=' + source_id;
+				nodeA.appendChild(textnode);
+				var nodeSource = document.createElement("DIV");
+				nodeSource.setAttribute("class", "sourceDesc");
+				nodeSource.style.color = "green";
+				nodeSource.appendChild(nodeA);
+				t.appendChild(nodeSource);
+
+//			    <div class="sourceDesc"><!-- Source {{ clist.grouper }} -->
+//		        {% set c = clist.list[0] %}
+//		        {% if c.source_id %}{% set source = obj[c.source_id] %}
+//		                   <a href="/scene/source={{source.uniq_id}}" class="inlink"
+//		                      title="[{{c.id}}] {{ _('See source %(name)s details', name=source.id) }}">
+//		                      {{source.stitle}}</a>
+//			            <!-- Repository -->
+//			            {% if source.repositories %} –
+//			               <span class="typedesc">{{ c.source_medium|transl('medium') }}</span>
+//			               <i>{{obj[source.repositories[0]].rname}}</i>
+//		                   {% if source.repositories|length > 1 %}<b> JA MUITA ARKISTOJA</b>{% endif %}
+//			            {% else %}  <b title="{{source}}">{{ _("No archive information!") }}</b>
+//			            {% endif %}
+//		        {% else %}<b title="{{c}}">{{ _("No source information!") }}</b>
+//		        {% endif %}
+			}
+			// Show Citations
+			for (j=0; j<line[1].length; j++) {
+				var mark = this.getMark(i, j);
+				var nodeB = document.createElement("B");
+				var textnode = document.createTextNode(mark + ' ');
+				nodeB.appendChild(textnode);
+				
+				var nodeA = document.createElement("SPAN");
+				nodeA.appendChild(nodeB);
+				var textnode = document.createTextNode("Page X");
+				nodeA.appendChild(textnode);
+
+				var nodeB = document.createElement("DIV");
+				nodeB.setAttribute("class", "citaDesc");
+				nodeB.id = "sref" + mark;
+				nodeB.appendChild(nodeA);
+
+				nodeSource.appendChild(nodeB);
+				
+//			 <div class="citaDesc" id="sref{{ cita.mark|trim }}" ...>
+//	            <span title="[{{cita.id}}] {{cita.confidence|transl("conf")}} {{ _('confidence') }} ({{cita.confidence}})">
+//	                <b>{{ cita.mark }}</b> {{ _("Page") }}: {{cita.page}} &nbsp; {{ macro.stars(conf) }}
+//	            </span> {{ cita.noteref }}
+//	          {% for nref in cita.note_ref %} &nbsp;►&nbsp;{{ macro.notelink(obj[nref]) }}
+//	          {% endfor %}
+//	         </div>;
+			}
+		}
+		console.log("Citation table=" + this.cTbl);
 	}
 }
 
