4feade34c26d2675f556078b655255c3013a91ba
Juha <juha.makelainen@iki.fi>
2020-02-16 16:09:33 +0200

    Fix index for handle
    + Fixed batch merge
    + media.html: link to fullsceen pic

--- app/bp/audit/models/cypher_audit.py
+++ app/bp/audit/models/cypher_audit.py
@@ -8,16 +8,13 @@ class Cypher_audit():
     ' Cypher clauses for auditor'
 
 # #Todo: Remove limit?
-#     copy_batch_to_audition = '''
-# MERGE (root:Audit {id:$batch, user:$user, auditor:$oper})
-#     SET root.timestamp = timestamp()
-# WITH root
-#     MATCH (b:Batch {id:$batch}) -[o:OWNS|OWNS_OTHER]-> (x)
-#     WITH root, o, b, x LIMIT 3000
-#         DELETE o
-#         CREATE (root) -[:PASSED]-> (x)
-#         RETURN x'''
+    copy_batch_to_audition = '''
+MERGE (root:Audit {id:$batch, user:$user, auditor:$oper})
+    SET root.timestamp = timestamp()
+WITH root
+    MATCH (b:Batch {id:$batch}) -[o:OWNS|OWNS_OTHER]-> (x)
+    WITH root, o, b, x LIMIT 3000
+        DELETE o
+        CREATE (root) -[:PASSED]-> (x)
+        RETURN x'''
    
-#     remove_my_nodes = """
-# MATCH (u:UserProfile) -[*]-> (a) WHERE u.username=$user
-# DETACH DELETE a"""
