e605e07385e3b84410f3fa346952f324de887bc1
Juha <juha.makelainen@iki.fi>
2020-04-28 17:50:36 +0300

    Logging to /tmp/stkserver
    - Todo: Configure logging directory
    - Todo: Only simulated user name is shown

--- run.py
+++ run.py
@@ -1,7 +1,39 @@
 # coding=UTF-8
 import logging
+
+class ContextFilter(logging.Filter):
+    """
+    This is a filter which injects contextual information into the log.
+
+    Rather than use actual contextual information, we just use random
+    data in this demo.
+    """
+
+    USERS = ['mari', 'li', 'katri']
+
+    def __init__(self):
+        from random import choice
+        self.user = choice(ContextFilter.USERS)
+
+    def filter(self, record):
+        record.user = self.user #choice(ContextFilter.USERS)
+        return True
+
+print("Start loggers here")
 logging.basicConfig(level=logging.INFO, format=('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger = logging.getLogger('stkserver')
+fh = logging.FileHandler('/tmp/stkserver.log')
+fh.setLevel(logging.DEBUG)
+logger.addFilter(ContextFilter())
+formatter = logging.Formatter('%(asctime)s %(name)s %(user)-7s %(levelname)-5s %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+# logger.basicConfig(level=logging.DEBUG,
+#                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+#                     datefmt='%m-%d %H:%M',
+#                     filename='stkserver.log',
+#                     filemode='w')
 #logger.setLevel(logging.DEBUG)
 
 # Do not log neo4j.bolt INFO messages
