40d24cd19a154ac8c087b9a83040193e011b0565
Juha Takala <juha.takala+rauta@iki.fi>
2020-05-06 20:10:35 +0300

    logging definition moved from run.py into app/__init__.py
    Logdirecory and file name given in instance/config.py as
    STK_LOGDIR = 'xxx'
    STK_LOGFILE = 'xxx'
    Logdirectory created if it does not exist, die if can not.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask, session, request
 from flask_babelex import Babel
 from flask_security import current_user
 import shareds
+
+import logging
+import os
+
 # Create app
 app = Flask(__name__, instance_relative_config=True)
 shareds.app = app
@@ -10,6 +14,47 @@ print('Application instance path: ' + shareds.app.instance_path)
 
 shareds.app.config.from_object('config')
 shareds.app.config.from_pyfile('config.py')
+
+class ContextFilter(logging.Filter):
+    """
+    This is a filter which injects contextual information into the log.
+    """
+
+    def __init__(self):
+        self.user = "<Nobody>" # choice(ContextFilter.USERS)
+
+    def filter(self, record):
+        if hasattr(self,'user'):
+            record.user = self.user
+        else:
+            record.user = '-'
+            print("# setups.ContextFilter.filter: 'user' not defined")
+        return True
+
+def setup_logging():
+    try:
+        logdir = shareds.app.config['STK_LOGDIR']
+        logfile = shareds.app.config['STK_LOGFILE']
+        if not os.path.isdir(logdir):
+            os.makedirs(logdir)
+    except Exception as e:
+        print("Can't create logging directory: {}".format(logdir, e))
+        exit(1)
+
+    formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(user)s %(message)s')
+
+    fh = logging.FileHandler(logdir + '/' + logfile)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+
+    logger = logging.getLogger('stkserver') # this name is hardcoded in other places also
+    logger.setLevel(logging.DEBUG)
+    logger.addFilter(ContextFilter())
+    logger.addHandler(fh)
+    print('Käynnistys: {} logging {} file {}'.format(app, logger, fh.stream.name))
+    return
+
+setup_logging()
 
 shareds.babel = Babel(shareds.app)
 #-----------------------------------------------------------------------------
@@ -56,7 +101,7 @@ def get_locale():
     reqlang = request.args.get('lang')
     if reqlang:
         session['lang'] = reqlang
-    else:    
+    else:
         reqlang = session.get('lang')
         if not reqlang:
             if current_user.is_authenticated: 
@@ -70,14 +115,14 @@ from models import syslog
 def log_user_logged_in(sender, user, **extra):
     syslog.log(type="user logged in")
     session['lang'] = current_user.language
-    
+
 def log_user_logged_out(sender, user, **extra):
     syslog.log(type="user logged out")
 
-import logging
 syslog.syslog_init()
 syslog.log(type="application initialized")
+
 user_logged_in.connect(log_user_logged_in,shareds.app)
 user_logged_out.connect(log_user_logged_out,shareds.app)
- 
+
 import setups
--- run.py
+++ run.py
@@ -1,59 +1,18 @@
 #!/usr/bin/python
 # coding=UTF-8
 
-
-import sys
 import os
 import logging
 
-class ContextFilter(logging.Filter):
-    """
-    This is a filter which injects contextual information into the log.
-    """
-
-    def __init__(self):
-        self.user = "<Nobody>" # choice(ContextFilter.USERS)
-
-    def filter(self, record):
-        if hasattr(self,'user'):
-            record.user = self.user
-        else:
-            record.user = '-'
-            print("# setups.ContextFilter.filter: 'user' not defined")
-        return True
-
-formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(user)s %(message)s')
-
-# Be clever about log file location:
-# Are we running on production/test server or developer enviroment?
-server_logdir = '/var/log/httpd/stkserver'
-running_on_server = os.path.isdir(server_logdir)
-if running_on_server:
-    fh = logging.FileHandler(server_logdir + '/stkserver.log')
-else:
-    # Developer enviroment, use env var STK_LOGDIR or /tmp
-    dev_logdir = os.environ.get("STK_LOGDIR")
-    if dev_logdir is not None:
-        fh = logging.FileHandler(dev_logdir + '/stkserver.log')
-    else:
-        fh = logging.FileHandler('/tmp/stkserver.log')
-    neo4j_log = logging.getLogger("neo4j.bolt")
-    neo4j_log.setLevel(logging.WARNING)
-
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-
-logger = logging.getLogger('stkserver')
-logger.setLevel(logging.DEBUG)
-logger.addFilter(ContextFilter())
-logger.addHandler(fh)
-
-if running_on_server:
+if os.geteuid() < 1000:
+    import sys
     sys.path.insert(0, os.path.join(os.getcwd(),"app"))
     from app import app as application
     application.secret_key = "You don't know OUR secret key"
 else:
     if __name__ == '__main__':
+        neo4j_log = logging.getLogger("neo4j.bolt")
+        neo4j_log.setLevel(logging.WARNING)
         from app import app
-        print('Käynnistys: {} logging {} file {}'.format(app, logger, fh.stream.name))
+        # print('Käynnistys: {} logging {} file {}'.format(app, logger, fh.stream.name))
         app.run()
