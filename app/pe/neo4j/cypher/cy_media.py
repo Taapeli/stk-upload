f21229d310277d3bc21608e5f87c2280bd235545
Kari Kujansuu <karikujansuu@gmail.com>
2021-06-21 12:23:58 +0300

    Implemented "run_cypher" for Media

--- app/pe/neo4j/cypher/cy_media.py
+++ app/pe/neo4j/cypher/cy_media.py
@@ -28,30 +28,18 @@ class CypherMedia():
 
 # Read Media data
 
-    get_my_media_by_uuid = """
-MATCH (:UserProfile {username:$user}) -[:HAS_ACCESS]-> (b) -[ro:OWNS]->
-      (media:Media {uuid:$uuid}) <-[r:MEDIA]- (ref)
-OPTIONAL MATCH (ref) <-[:EVENT]- (ref2)
-RETURN media, PROPERTIES(r) AS prop, ref, ref2"""
-    get_approved_media_by_uuid = """
-MATCH () -[ro:OWNS|PASSED]-> (media:Media {uuid:$uuid}) <-[r:MEDIA]- (ref)
+    get_media_by_uuid = """
+MATCH (root) -[:OBJ_OTHER]-> (media:Media {uuid:$uuid}) <-[r:MEDIA]- (ref)
 OPTIONAL MATCH (ref) <-[:EVENT]- (ref2)
 RETURN media, PROPERTIES(r) AS prop, ref, ref2"""
 
     get_all = "MATCH (o:Media) RETURN o"
 
     # Media list by description with count limit
-    read_approved_medias = """
-MATCH (prof) -[:PASSED]-> (o:Media) <- [r:MEDIA] - () 
-WHERE o.description >= $start_name 
-RETURN o, prof.user as credit, prof.id as batch_id, COUNT(r) AS count
-    ORDER BY o.description LIMIT $limit"""
-
-    read_my_medias = """
-MATCH (u:UserProfile {username:$user}) -[:HAS_ACCESS]-> (b:Batch)
-    -[owner:OWNS]-> (o:Media) <- [r:MEDIA] - ()
+    get_media_list = """
+MATCH (root) -[:OBJ_OTHER]-> (o:Media) <- [r:MEDIA] - ()
 WHERE o.description >= $start_name
-RETURN o, b.user as credit, b.id as batch_id, COUNT(r) AS count
+RETURN o, case root.original_user when null then root.user else root.original_user end as credit, root.id as batch_id, COUNT(r) AS count
     ORDER BY o.description LIMIT $limit"""
 
 
