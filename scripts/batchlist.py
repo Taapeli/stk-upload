08f0ef3aedc890eb253b3e4f94202c213c66a3a8
jpek-m <jpek@iki.fi>
2021-08-02 15:56:14 +0300

    Comment list detects unknown type

--- scripts/batchlist.py
+++ scripts/batchlist.py
@@ -14,7 +14,8 @@ from unittest.mock import Mock
 
 sys.path.append("../app")
 import shareds
-from bl.batch import Batch
+from bl.root import Root
+#from bl.batch import Batch
  
 def load_config(configfile):
     shareds.app = Mock()
@@ -36,13 +37,12 @@ shareds.db = Neo4jEngine(shareds.app)
 shareds.driver  = shareds.db.driver
 
 print(f"{'id':14s} {'user':10s}  {'material_type':15}  {'description':15} {'status':15s} {'file'}")
-for b in Batch.get_batches():
+for b in Root.get_batches():
     id = b['id']
-    status = b['status']
-    file = b['file']
-    user = b['user']
+    state = b.get('state',"-")
+    file = b.get('file',"-")
+    user = b.get('user',"-")
     material_type = b.get('material_type','')
     description = b.get('description','').replace("\n"," ")
-    print(f"{id:14s} {user:10s}  {material_type:15}  {description[0:15]:15} {status:15s} {file}")
- 
+    print(f"{id:14s} {user:10s}  {material_type:15}  {description[0:15]:15} {state:15s} {file}")
  
