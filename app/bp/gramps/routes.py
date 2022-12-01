1dafd351a12e880c74c75c559452ca58609c13c7
Kari Kujansuu <kari.kujansuu@gmail.com>
2022-12-01 18:41:48 +0200

    Add config variable GRAMPS_ISOTAMMI_ID_TOOL

--- app/bp/gramps/routes.py
+++ app/bp/gramps/routes.py
@@ -234,7 +234,10 @@ def gramps_isotammi_ids(batch_id):
 def gramps_isotammi_ids_execute(batch_id, newfile):
     lang = session.get("lang","")
     print("lang",lang)
-    msgs = gramps_utils.gramps_run_for_batch(shareds.app,  "isotammi-ids", lang, current_user.username, batch_id, newfile)
+    toolname = shareds.app.config.get("GRAMPS_ISOTAMMI_ID_TOOL")
+    if not toolname:
+        toolname = "isotammi-ids"
+        msgs = gramps_utils.gramps_run_for_batch(shareds.app, toolname, lang, current_user.username, batch_id, newfile)
     
     rsplines = []
     for line in  msgs:
